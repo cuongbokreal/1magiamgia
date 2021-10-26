54e9685cd8ac583812dae639d903cef56d3819fc
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-10-26 16:58:16 +0700

    Create main.js

---
+++ go/main.js
@@ -0,0 +1,27 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+ 
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+var url = getUrlParameter('url');
+var urlAtob = window.atob(url);
+const id = '5353514789844343379';
+var urlFinal = `https://go.isclix.com/deep_link/${id}?url=${urlAtob}&utm_source=trumGiamGia&utm_medium=deeplink`;
+  window.location.replace(urlFinal);
+var refresh = document.createElement("META");
+refresh.setAttribute("http-equiv", "refresh");
+refresh.setAttribute("content", `0;url=${urlFinal}`);
+var ogUrl = document.createElement("META");
+ogUrl.setAttribute("property", "og:url");
+ogUrl.setAttribute("content", `${urlFinal}`);
+document.head.appendChild(refresh);
+document.head.appendChild(ogUrl);
+
